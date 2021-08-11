c2e6c081970d34b99fac19819b15244784f63f99
Mike! <mike@ensor.cc>
2021-08-10 22:54:50 -0700

    adding options for host prefixing

--- create-isos.sh
+++ create-isos.sh
@@ -10,14 +10,16 @@ function show_help() {
     echo "    -c [#]                    Count of hosts to create"
     echo "    -l [<path>]               Path to write the ISOs into (defaults to '.')"
     echo "    -d                        Dry-run, print out what commands would be"
+    echo "    -n <name>                 Prefix for hostname (Default: nuc)"
     echo ""
 }
 
 HOST_COUNT=6
 DRY_RUN=0
 OUTPUT_PATH="."
+HOST_PREFIX="nuc"
 
-while getopts "h?dc:" opt; do
+while getopts "h?dl:c:n:" opt; do
     case "$opt" in
     h|\?)
         show_help
@@ -26,17 +28,18 @@ while getopts "h?dc:" opt; do
     c)  HOST_COUNT="$OPTARG";;
     d)  DRY_RUN="1";;
     l)  OUTPUT_PATH="$OPTARG";;
+    n)  HOST_PREFIX="$OPTARG";;
     esac
 done
 shift $(($OPTIND-1))
 
 for (( host=1; host<=$HOST_COUNT; host++ ))
 do
-    echo "Creating 'nuc-$host' ISO image..."
+    echo "Creating '${HOST_PREFIX}-$host' ISO image..."
     if [[ ${DRY_RUN} > 0 ]]; then
-        echo -e "dry-run->\t./build_iso.sh -u abm-admin -H nuc-${host} -F ${OUTPUT_PATH}/nuc-${host}.iso\n"
+        echo -e "dry-run->\t./build_iso.sh -u abm-admin -H ${HOST_PREFIX}-${host} -F ${OUTPUT_PATH}/${HOST_PREFIX}-${host}.iso\n"
     else
-        ./build_iso.sh -u abm-admin -H nuc-${host} -F ${OUTPUT_PATH}/nuc-${host}.iso
+        ./build_iso.sh -u abm-admin -H ${HOST_PREFIX}-${host} -F ${OUTPUT_PATH}/${HOST_PREFIX}-${host}.iso
     fi
 done
 
